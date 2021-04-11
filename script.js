65895249e24cfec12a780f6b176a491ed76398f6
Wetherk <Wetherk@yandex.by>
2021-04-11 22:05:16 +0300

    a little bit of work

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("ez");
+const albumContainer = document.getElementById("album-container");
+let dataArr = [];
+fetch("https://jsonplaceholder.typicode.com/albums")
+	.then((response) => response.json())
+	.then((data) => (dataArr = [...data]))
+	.then(() => {
+		console.log(dataArr[0].title);
+		for (let i = 0; i < 8; i++) {
+			const block = document.createElement("div");
+			const text = document.createTextNode(dataArr[i].title);
+			block.appendChild(text);
+			albumContainer.appendChild(block);
+		}
+	});
